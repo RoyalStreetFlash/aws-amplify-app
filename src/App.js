8d3df1044f73fb5da5a6c76f310dc06da4896875
RoyalStreetFlash <y.kakazu8020@gmail.com>
2024-11-21 00:26:07 +0900

    ビルド試し

--- src/App.js
+++ src/App.js
@@ -7,6 +7,7 @@ import "@aws-amplify/ui-react/styles.css";
 import awsExports from "./aws-exports";
 Amplify.configure(awsExports);
 
+// ビルド試すためのコメント
 function App({ signOut, user }) {
   return (
     <div className="App">
